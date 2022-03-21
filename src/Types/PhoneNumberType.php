2ec0cbe752a96d83c811ec58fedfe1fed076fcd3
Benjamin Morel <benjamin.morel@gmail.com>
2022-03-22 00:06:23 +0100

    Add return types

--- src/Types/PhoneNumberType.php
+++ src/Types/PhoneNumberType.php
@@ -11,12 +11,12 @@ use Doctrine\DBAL\Types\Type;
 
 final class PhoneNumberType extends Type
 {
-    public function getName()
+    public function getName() : string
     {
         return 'PhoneNumber';
     }
 
-    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    public function convertToDatabaseValue($value, AbstractPlatform $platform) : ?string
     {
         if ($value === null) {
             return null;
@@ -29,7 +29,7 @@ final class PhoneNumberType extends Type
         throw ConversionException::conversionFailedInvalidType($value, $this->getName(), [PhoneNumber::class, 'null']);
     }
 
-    public function convertToPHPValue($value, AbstractPlatform $platform)
+    public function convertToPHPValue($value, AbstractPlatform $platform) : ?PhoneNumber
     {
         if ($value === null) {
             return null;
@@ -42,7 +42,7 @@ final class PhoneNumberType extends Type
         throw ConversionException::conversionFailedInvalidType($value, $this->getName(), ['string', 'null']);
     }
 
-    public function getSQLDeclaration(array $column, AbstractPlatform $platform)
+    public function getSQLDeclaration(array $column, AbstractPlatform $platform) : string
     {
         // E.164 defines the maximum length as 15 digits, to which we add 1 char for the leading + sign.
         if (!isset($column['length'])) {
@@ -52,7 +52,7 @@ final class PhoneNumberType extends Type
         return $platform->getVarcharTypeDeclarationSQL($column);
     }
 
-    public function requiresSQLCommentHint(AbstractPlatform $platform)
+    public function requiresSQLCommentHint(AbstractPlatform $platform) : bool
     {
         return true;
     }
